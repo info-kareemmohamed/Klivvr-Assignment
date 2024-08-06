c696ceebdb1e56844b3e68955909b26426c017af
Kareem Mohamed <63722@fa-hiss.edu.eg>
2024-08-06 22:12:46 +0300

    update home screen design

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,6 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     id("com.google.dagger.hilt.android") version "2.50" apply false
-    id ("org.jetbrains.kotlin.plugin.serialization") version "1.8.0" apply false
+    kotlin("jvm") version "2.0.0" apply false
 
 }
