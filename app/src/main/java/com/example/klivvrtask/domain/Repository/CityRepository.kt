6ece1b6397053ab98ba5880823d755779a563682
Kareem Mohamed <63722@fa-hiss.edu.eg>
2024-08-08 00:08:13 +0300

    repository  : City repository

---
+++ app/src/main/java/com/example/klivvrtask/domain/Repository/CityRepository.kt
@@ -0,0 +1,9 @@
+package com.example.klivvrtask.domain.Repository
+
+import com.example.klivvrtask.common.Resource
+import com.example.klivvrtask.domain.model.City
+import kotlinx.coroutines.flow.Flow
+
+interface CityRepository {
+    fun getCities(): Flow<Resource<List<City>>>
+}
