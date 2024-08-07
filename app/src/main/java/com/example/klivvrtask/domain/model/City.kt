6c82c6fce42403f99c0e97f1a3a714ca06301cad
Kareem Mohamed <63722@fa-hiss.edu.eg>
2024-08-07 23:57:23 +0300

    model

---
+++ app/src/main/java/com/example/klivvrtask/domain/model/City.kt
@@ -0,0 +1,9 @@
+package com.example.klivvrtask.domain.model
+
+data class City(
+    val id: Int,
+    val name: String,
+    val country: String,
+    val latitude: Double,
+    val longitude: Double
+)
