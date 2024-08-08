b5bd07b5afe08aa2e62b0abb010db444f903eadc
Kareem Mohamed <63722@fa-hiss.edu.eg>
2024-08-08 15:16:45 +0300

    use case : Sort cities

---
+++ app/src/main/java/com/example/klivvrtask/domain/use_case/SortCitiesUseCase.kt
@@ -0,0 +1,26 @@
+package com.example.klivvrtask.domain.use_case
+
+import com.example.klivvrtask.domain.model.City
+
+/**
+ * Sorts a list of cities based on the specified criteria.
+ *
+ * @param cities The list of cities to be sorted.
+ * @return A list of cities sorted by their starting character, with non-letter starting cities at the end.
+ */
+class SortCitiesUseCase {
+
+    operator fun invoke(cities: List<City>): List<City> {
+        val sortedCities = cities.sortedWith { city1, city2 ->
+            val isSpecial1 = city1.name.firstOrNull()?.let { !it.isLetter() } == true
+            val isSpecial2 = city2.name.firstOrNull()?.let { !it.isLetter() } == true
+
+            when {
+                isSpecial1 && !isSpecial2 -> 1
+                !isSpecial1 && isSpecial2 -> -1
+                else -> city1.name.lowercase().compareTo(city2.name.lowercase())
+            }
+        }
+        return sortedCities
+    }
+}
